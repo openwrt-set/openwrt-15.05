0c5f2a358ab5bb2afaef82ef3090e0b4b1aa8964
dragonzx <dragonzx@yandex.ru>
2019-06-28 04:37:30 -0700

    Fix unresolved links

--- include/download.mk
+++ include/download.mk
@@ -5,7 +5,7 @@
 # See /LICENSE for more information.
 #
 
-OPENWRT_GIT = http://git.openwrt.org
+OPENWRT_GIT = https://github.com/openwrt-set
 
 DOWNLOAD_RDEP=$(STAMP_PREPARED) $(HOST_STAMP_PREPARED)
 
